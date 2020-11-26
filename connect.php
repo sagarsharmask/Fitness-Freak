b7b284e06a489dd3d73f8d572ea8299e5d140625
Ashish Ranjan <ashish.kumar_cs18@gla.ac.in>
2020-11-26 22:49:19 +0530

    Add files via upload
    project

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+   $Name = $_POST['Name'];
+   $Age = $_POST['Age'];
+   $Gender = $_POST['Gender'];
+   $Locality = $_POST['Locality'];
+   $Email = $_POST['Email'];
+   $Number = $_POST['Number'];
+   
+   $conn = mysqli_connect('127.0.0.1:3307','root','','testp');
+   if(!$conn){
+       die('Connection Failed  : '.mysqli_connect_error());
+    //    console.log('test');
+   }else{
+       $stmt = $conn->prepare("insert into registration(Name,Age,gender,Locality,Email,Number)
+       values(?,?,?,?,?,?)");
+       $stmt->bind_param("sisssi",$Name,$Age,$Gender,$Locality,$Email,$Number);
+       $stmt->execute();
+       echo "registration successfully...";
+       $stmt->close();
+       $conn->close();
+   }
+?>
